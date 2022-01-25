a0763b3a7ab4c22eb22b429596d1e30380e12d25
riesenfeld <aron.riesenfeld@gmail.com>
2022-01-24 23:21:08 -0800

    added new DualTypes view and TypeButton component

---
+++ src/components/TypeButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="type-button" :style="colorMap" @click="handleClickEvent">{{ typeName }}</button>
+</template>
+
+<script>
+export default {
+  name: "TypeButton",
+  props: {
+    typeName: String,
+    typeColors: Array,
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    handleClickEvent() {
+      this.$emit("buttonClicked", this.typeName)
+    },
+  },
+  computed: {
+    colorMap() {
+      return {
+        backgroundColor: this.typeColors[2],
+        color: this.typeColors[1],
+        filter: "contrast(130%)",
+        padding: "1px",
+        borderRadius: "2px",
+      }
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 import Home from "../views/Home.vue"
+import DualTypes from "../views/DualTypes.vue"
 import pokemonTypesGen6 from "@/PokemonTypesGen6.js"
 import pokemonTypesGen2 from "@/PokemonTypesGen2.js"
 import pokemonTypesGen1 from "@/PokemonTypesGen1.js"
@@ -18,7 +19,18 @@ const routes = [
         gen2: pokemonTypesGen2,
         gen6: pokemonTypesGen6,
       },
-      // selectedGeneration: selectedGeneration,
+    },
+  },
+  {
+    path: "/dual-types",
+    name: "DualTypes",
+    component: DualTypes,
+    props: {
+      typesByGeneration: {
+        gen1: pokemonTypesGen1,
+        gen2: pokemonTypesGen2,
+        gen6: pokemonTypesGen6,
+      },
     },
   },
   {
