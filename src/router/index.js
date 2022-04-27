6f070d578e70f59f466bd28358945703361207d8
riesenfeld <aron.riesenfeld@gmail.com>
2022-04-26 17:16:07 -0700

    rolling back most of previous commit, which belongs in a different feature branch

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 import Home from "../views/Home.vue"
-import DualTypes from "../views/DualTypes.vue"
+// import DualTypes from "../views/DualTypes.vue"
 import pokemonTypesGen6 from "@/PokemonTypesGen6.js"
 import pokemonTypesGen2 from "@/PokemonTypesGen2.js"
 import pokemonTypesGen1 from "@/PokemonTypesGen1.js"
@@ -13,18 +13,6 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
-    props: {
-      typesByGeneration: {
-        gen1: pokemonTypesGen1,
-        gen2: pokemonTypesGen2,
-        gen6: pokemonTypesGen6,
-      },
-    },
-  },
-  {
-    path: "/dual-types",
-    name: "DualTypes",
-    component: DualTypes,
     props: {
       typesByGeneration: {
         gen1: pokemonTypesGen1,
